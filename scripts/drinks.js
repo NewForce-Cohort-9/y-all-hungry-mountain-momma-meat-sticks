be65805bb98549df956fd1811058bca905b50efe
tylerhamilton888 <tyler.hamilton978@gmail.com>
2024-05-16 10:04:35 -0400

    fixed the tracker and orders recorder

--- scripts/drinks.js
+++ scripts/drinks.js
@@ -1,10 +1,15 @@
 import { updateTotalPrice } from './runningPrice.js';
+import { setDrinkChoice } from './transientState.js';
 
 const handleDrinksChoice = (event) => {
     if (event.target.id === "drink") {
+        setDrinkChoice(parseInt(event.target.value));
         updateTotalPrice(); // Update the total price whenever a drink is selected
     }
 };
+
+document.addEventListener("change", handleDrinksChoice)
+
 
 export const drinkOptions = async () => {
     const response = await fetch("http://localhost:8088/drinks");
@@ -15,7 +20,7 @@ export const drinkOptions = async () => {
     drinksOptionsHTML += `<option value="0">Drinks Items</option>`;
     drinksOptionsHTML += `<option value="none">None</option>`;
     for (const drink of drinks) {
-        drinksOptionsHTML += `<option value="${drink.price}" data-price="${drink.price}">${drink.name}</option>`;
+        drinksOptionsHTML += `<option value="${drink.id}" data-price="${drink.price}">${drink.name}</option>`;
     }
     drinksOptionsHTML += `</select>`;
 
